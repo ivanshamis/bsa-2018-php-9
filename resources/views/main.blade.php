e9fb7123a49d7a6ceac6d1cca094befb46040dc0
ivanshamis <ivanshamis@gmail.com>
2018-07-14 17:20:07 +0300

    task0-2

---
+++ resources/views/main.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('title', 'Currency market')
+
+@section('header')
+
+@endsection
+
+@section('content-title', 'Currency market')
+
+@section('content') 
+
+@endsection
