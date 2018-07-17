bce57fa3f54664b608da53a201994fd2809e3516
ivanshamis <ivanshamis@gmail.com>
2018-07-18 00:28:00 +0300

    fixes

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -25,74 +25,62 @@ class CurrencyController extends Controller
     public function show(int $id)
     {
         $currency = Currency::find($id);
-        if (Gate::allows('currency.view', $currency)) {
-            return view('currencies-show', [
-                'currencies' => $this->getAllCurrencies(),
-                'currency' => $currency
-            ]);
-        }
-        else {
+        if (Gate::denies('currency.view', $currency)) {
             return redirect()->route('main');
         }
+        return view('currencies-show', [
+            'currencies' => $this->getAllCurrencies(),
+            'currency' => $currency
+        ]);
     }
 
     public function create()
     {
-        if (Gate::allows('currency.create', Currency::class)) {
-            return view('currencies-add', [ 'currencies' => $this->getAllCurrencies()]);
-        }
-        else {
+        if (Gate::denies('currency.create', Currency::class)) {
             return redirect()->route('main');
         }
+        return view('currencies-add', [ 'currencies' => $this->getAllCurrencies()]);
     }
 
     public function edit(int $id)
     {
         $currency = Currency::find($id);
-        if (Gate::allows('currency.update', $currency)) {
-            return view('currencies-edit', [
-            'currencies' => $this->getAllCurrencies(),
-            'currency' => $currency
-            ]);
-        }
-        else {
+        if (Gate::denies('currency.update', $currency)) {
             return redirect()->route('main');
         }
+        return view('currencies-edit', [
+            'currencies' => $this->getAllCurrencies(),
+            'currency' => $currency
+        ]);
     }
 
     public function destroy(int $id)
     {
         $currency = Currency::find($id);
-        if (Gate::allows('currency.delete', $currency)) {
-            Currency::destroy($id);
-            return redirect()->route('currencies.index');
-        }
-        else {
+        if (Gate::denies('currency.delete', $currency)) {
             return redirect()->route('main');
         }
+        Currency::destroy($id);
+        return redirect()->route('currencies.index');
     }
 
     public function store(CurrencyRequest $request)
     {   
-        if (Gate::allows('currency.create', Currency::class)) {
-            $currency = Currency::create($request->all());
-            return redirect()->route('currencies.index');
-        }
-        else {
+        if (Gate::denies('currency.create', Currency::class)) {
             return redirect()->route('main');
         }
+        $currency = Currency::create($request->all());
+        return redirect()->route('currencies.index');
     }
 
     public function update(int $id, CurrencyRequest $request)
     {   
         $currency = Currency::find($id);
-        if (Gate::allows('currency.update', $currency)) {
-            $currency->fill($request->all());
-            $currency->save();
-            return redirect()->route('currencies.show',['id' => $currency->id]);
-        }
-        else {
+        if (Gate::denies('currency.update', $currency)) {
             return redirect()->route('main');
         }
+        $currency->fill($request->all());
+        $currency->save();
+        return redirect()->route('currencies.show',['id' => $currency->id]);
     }
 }
